07cf788cf2adb849a3fd936ef72b436685da59f3
charliepreece95 <charlieopreece@hotmail.co.uk>
2018-08-22 04:02:04 +0100

    using objects

---
+++ TeamStatus.js
@@ -0,0 +1,59 @@
+//empty objects
+const team = {
+  _players: [],
+  _games: [],
+  _scores: [],
+  //get players
+  get players() {
+    return this._players;
+  },
+  //get games
+  get games() {
+    return this._games;
+  },
+  get scores() {
+    return this._scores;
+  },
+  addScores(homeScore, awayScore) {
+    let score = {
+      homeScore: homeScore,
+      awayScore: awayScore
+    };
+    this.scores.push(score);
+  },
+  //add player to the object
+  addPlayer(firstname, lastname, age, shirtNo) {
+    let player = {
+      firstname: firstname, 
+      lastname: lastname, 
+      age: age, 
+      shirtNo: shirtNo
+    };
+    this.players.push(player);   
+ },
+  //add game to the object
+  addGame(opponent, teamPoints, opponentPoints) {
+    let game = {
+      opponent: opponent, 
+      teamPoints: teamPoints, 
+      opponentPoints: opponentPoints
+    };
+    this.games.push(game);
+  }
+};
+//add player
+team.addPlayer('Joe', 'Bloggs', 23, 9);
+team.addPlayer('John', 'Smith', 50, 10);
+team.addPlayer('Ben', 'Jones', 18, 22);
+//add game
+team.addGame('Inter Milan', 44, 30);
+team.addGame('Ac Milan', 44, 50);
+team.addGame('Roma', 44, 20);
+
+team.addScores(2, 1);
+team.addScores(3, 3);
+team.addScores(0, 1);
+//log to the console
+console.log(team.players);
+console.log(team.games);
+console.log(team.scores);
